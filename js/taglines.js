df08a23dd35325a9a355bd5c4bc8b2b0e7a7aff3
Mike Zazaian <mk@mkzzn.org>
2010-06-15 13:13:18 -0400

    add small javascript for applying random taglines

---
+++ js/taglines.js
@@ -0,0 +1,20 @@
+function randomTagline() {
+  taglines = [
+    "terrifying and illogical",
+    "mind-altering and child-pulverizing",
+    "soul-decanting and water-divining",
+    "condor-waffling and mudhen-assailing",
+    "rhododendron-abstracting and Bobby-Fischer-defeating",
+    "Ben-Kingsley-disarming and parking-lot-obliterating",
+    "UN-undermining and mountain-goat-arbitrating",
+    "diplomat-catapulting and marmoset-confounding",
+    "skillful-brick-laying and crocodile-annihilating",
+    "salary-cap-defying and ostrich-ridiculing"
+  ]
+  
+  return taglines[Math.floor(Math.random()*taglines.length)]
+}
+
+jQuery(function($) {
+  $("#logo-tagline").html(randomTagline());
+});
