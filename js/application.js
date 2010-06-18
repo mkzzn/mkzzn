75d77be9da6de5ecbe9c14a670ac71af19b9e3b1
Mike Zazaian <mk@mkzzn.org>
2010-06-18 17:19:48 -0400

    get rid of adverbs again, add some other nouns/adjectives

--- js/application.js
+++ js/application.js
@@ -1,5 +1,4 @@
 window.onload = function() {
   document.getElementById("tagline-adjective").innerHTML = randomArrayMember(tagline["adjectives"]);
   document.getElementById("tagline-noun").innerHTML = randomArrayMember(tagline["nouns"]);
-  document.getElementById("tagline-adverb").innerHTML = randomArrayMember(tagline["adverbs"]);
 }
